36a87032a067c5c5e6227670b39026e01b46ca39
Rich Jones <rich.jones@edo.co>
2018-12-11 14:56:39 +0000

    Google mail script – add comments

--- apps/web/google_mail.py
+++ apps/web/google_mail.py
@@ -2,6 +2,9 @@ from talon.voice import Context, Key, press
 from talon import ctrl
 import logging
 
+# Note: These keyboard shortcuts won't work unless keyboard shortcuts are turned on.
+# See https://support.google.com/mail/answer/6594?hl=en-GB
+
 ctx = Context(
     "google_mail",
     func=lambda app, win: "- Gmail" in win.title or "- Edo Mail" in win.title
